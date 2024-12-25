061768c6c914ff8dda9ccc48c61d251e4a1dafae
Yashraj Dighe <yashraj.dighe077@gmail.com>
2024-12-25 17:46:55 +0000

    fix aws_iam_policy_document reference

--- terraform/aws/eks_addons_iam_roles.tf
+++ terraform/aws/eks_addons_iam_roles.tf
@@ -32,7 +32,7 @@ data "aws_iam_policy_document" "aws_ebs_csi_driver_assume_role_document" {
 
 resource "aws_iam_role" "aws_ebs_csi_driver_assume_role" {
   name               = var.aws_ebs_csi_driver_role_name
-  assume_role_policy = data.aws_ebs_csi_driver_assume_role_document.assume_role.json
+  assume_role_policy = data.aws_iam_policy_document.aws_ebs_csi_driver_assume_role_document.json
 }
 
 resource "aws_iam_role_policy_attachment" "test-attach" {
