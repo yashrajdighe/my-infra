5d15332df6b2239eb457a180d1cc650e5ce46f02
Yashraj Dighe <yashraj.dighe077@gmail.com>
2024-12-26 07:34:38 +0000

    use manifest_decode_multi instead

--- terraform/aws/eks_metrics_server.tf
+++ terraform/aws/eks_metrics_server.tf
@@ -1,3 +1,3 @@
 resource "kubernetes_manifest" "metrics_server_manifest" {
-  manifest = provider::kubernetes::manifest_decode(data.http.metrics_server_manifest.response_body)
+  manifest = provider::kubernetes::manifest_decode_multi(data.http.metrics_server_manifest.response_body)
 }
