96b1e641f32f09afdddbb898197c9ed8ab6d302b
Yashraj Dighe <yashraj.dighe077@gmail.com>
2024-12-26 07:07:11 +0000

    added helm provider

--- terraform/aws/variables.tf
+++ terraform/aws/variables.tf
@@ -12,3 +12,8 @@ variable "aws_ebs_csi_driver_policy_arn" {
   default = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
   type    = string
 }
+
+variable "cluster_name" {
+  default = "common-eks"
+  type    = string
+}
