6013b98f4944f832dd1514bf891eb6c4d782dedd
Yashraj Dighe <yashraj.dighe077@gmail.com>
2024-12-27 10:42:08 +0000

    remove helm and k8s providers and data source

--- terraform/aws/data.tf
+++ terraform/aws/data.tf
@@ -1,5 +1,5 @@
 data "aws_caller_identity" "current" {}
 
-data "aws_eks_cluster" "eks" {
-  name = module.eks.cluster_name
-}
+# data "aws_eks_cluster" "eks" {
+#   name = module.eks.cluster_name
+# }
