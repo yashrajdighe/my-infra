c6551c304719c70cbade036949eb22d680d4e54a
Yashraj Dighe <yashraj.dighe077@gmail.com>
2024-12-27 10:39:14 +0000

    remove helm deployments

--- terraform/aws/eks_metrics_server.tf
+++ terraform/aws/eks_metrics_server.tf
@@ -1,8 +1,8 @@
-resource "helm_release" "metrics_server" {
-  name       = "metrics-server"
-  repository = "https://kubernetes-sigs.github.io/metrics-server/"
-  chart      = "metrics-server"
-  version    = "3.12.2"
+# resource "helm_release" "metrics_server" {
+#   name       = "metrics-server"
+#   repository = "https://kubernetes-sigs.github.io/metrics-server/"
+#   chart      = "metrics-server"
+#   version    = "3.12.2"
 
-  namespace = "kube-system"
-}
+#   namespace = "kube-system"
+# }
