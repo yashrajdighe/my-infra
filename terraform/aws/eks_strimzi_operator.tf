54d205c7dd8d26d43f8ba5ba6cda8c443713c694
Yashraj Dighe <yashraj.dighe077@gmail.com>
2024-12-27 10:35:20 +0000

    provision infra

--- terraform/aws/eks_strimzi_operator.tf
+++ terraform/aws/eks_strimzi_operator.tf
@@ -1,9 +1,14 @@
-# resource "helm_release" "strimzi_kafka_operator" {
-#   name       = "strimzi-operator"
-#   repository = "oci://quay.io/strimzi-helm/"
-#   chart      = "strimzi-kafka-operator"
-#   version    = "0.45.0"
+resource "kubernetes_namespace" "strimzi_kafka_operator" {
+  metadata {
+    name = var.strimzi_kafka_operator_namespace
+  }
+}
 
-#   namespace        = "strimzi-operator"
-#   create_namespace = true
-# }
+resource "helm_release" "strimzi_kafka_operator" {
+  name       = "strimzi-operator"
+  repository = "oci://quay.io/strimzi-helm/"
+  chart      = "strimzi-kafka-operator"
+  version    = "0.45.0"
+
+  namespace = kubernetes_namespace.strimzi_kafka_operator.metadata[0].name
+}
