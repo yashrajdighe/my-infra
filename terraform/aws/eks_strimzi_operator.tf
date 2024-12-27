597078841afa0924d54aadf04c0de7435ec96a1d
Yashraj Dighe <yashraj.dighe077@gmail.com>
2024-12-27 11:39:36 +0000

    remove helm deployments

--- terraform/aws/eks_strimzi_operator.tf
+++ terraform/aws/eks_strimzi_operator.tf
@@ -1,14 +1,14 @@
-resource "kubernetes_namespace" "strimzi_kafka_operator" {
-  metadata {
-    name = var.strimzi_kafka_operator_namespace
-  }
-}
+# resource "kubernetes_namespace" "strimzi_kafka_operator" {
+#   metadata {
+#     name = var.strimzi_kafka_operator_namespace
+#   }
+# }
 
-resource "helm_release" "strimzi_kafka_operator" {
-  name       = "strimzi-operator"
-  repository = "oci://quay.io/strimzi-helm/"
-  chart      = "strimzi-kafka-operator"
-  version    = "0.45.0"
+# resource "helm_release" "strimzi_kafka_operator" {
+#   name       = "strimzi-operator"
+#   repository = "oci://quay.io/strimzi-helm/"
+#   chart      = "strimzi-kafka-operator"
+#   version    = "0.45.0"
 
-  namespace = kubernetes_namespace.strimzi_kafka_operator.metadata[0].name
-}
+#   namespace = kubernetes_namespace.strimzi_kafka_operator.metadata[0].name
+# }
