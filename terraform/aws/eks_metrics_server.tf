7a047c415b101cfd7da30f7fa29f24ae1a858075
Yashraj Dighe <yashraj.dighe077@gmail.com>
2025-02-09 04:32:28 +0000

    provision infra

--- terraform/aws/eks_metrics_server.tf
+++
@@ -1,8 +0,0 @@
-# resource "helm_release" "metrics_server" {
-#   name       = "metrics-server"
-#   repository = "https://kubernetes-sigs.github.io/metrics-server/"
-#   chart      = "metrics-server"
-#   version    = "3.12.2"
-
-#   namespace = var.metrics_server_namespace
-# }
